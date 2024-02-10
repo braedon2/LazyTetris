5b071f97c2ab326f069bc239f29c578a570409f3
braedon <braedonmcdonald1@gmail.com>
2024-02-10 18:29:43 -0500

    bricks stack

---
+++ Makefile
@@ -0,0 +1,9 @@
+COMPILER = clang++
+SOURCE_LIBS = -Ilib/
+OSX_OPT = -std=c++17 -Llib/ -framework CoreVideo -framework IOKit -framework Cocoa -framework GLUT -framework OpenGL lib/libraylib.a
+OSX_OUT = -o "bin/build_osx"
+CPPFILES = src/*.cpp
+
+build_osx: 
+	$(COMPILER) $(CPPFILES) $(SOURCE_LIBS) $(OSX_OUT) $(OSX_OPT)
+	
--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
-#include "raylib.h"
-#include "raymath.h"
+#include <raylib.h>
+#include <raymath.h>
+#include <vector>
 
 class Timer {
     private:
@@ -18,22 +19,53 @@ class Timer {
         }
 };
 
+class BoardCell {
+    public:
+        Color color;
+        bool isEmpty;
+
+        BoardCell(Color _color, bool _isEmpty): color(_color), isEmpty(_isEmpty) {}
+};
+
 int main(void) { 
-    InitWindow(50, 250, "Template-4.0.0");
+    InitWindow(100, 250, "Template-4.0.0");
     Timer timer;
     SetTargetFPS(60);
     int pos = 0;
+    std::vector<BoardCell> board(10, BoardCell(BLANK, true));
 
     while (!WindowShouldClose()) {
-        if (timer.getElapsed() > 1 and pos < 9) {
+        if (timer.getElapsed() > 1) {
+            if (pos == 9) {
+                board[pos] = BoardCell(RED, false);
+                pos = 0;
+            }
+            else if (board[pos + 1].isEmpty) {
+                pos++;
+            }
+            else if (!board[pos].isEmpty) {
+                // game over
+            }
+            else {
+                board[pos] = BoardCell(RED, false);
+                pos = 0;
+            }
+
             timer.start();
-            pos++;
         }
 
         BeginDrawing();
             ClearBackground(RAYWHITE);
             DrawRectangle(0, 25 * pos, 25, 25, RED);
             DrawRectangleLines(0, 25 * pos, 25, 25, BLACK);
+
+            for (int i = 0; i < 10; i++) {
+                BoardCell cell = board[i];
+                if (!cell.isEmpty) {
+                    DrawRectangle(0, 25 * i, 25, 25, RED);
+                    DrawRectangleLines(0, 25 * i, 25, 25, BLACK);
+                }
+            }
         EndDrawing();
     }
     
