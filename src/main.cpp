9d76e1137a6a58f8cc5dc4394fb792de1f18f642
braedon <braedonmcdonald1@gmail.com>
2024-02-15 09:28:07 -0500

    add class GameGrid

--- src/main.cpp
+++ src/main.cpp
@@ -29,12 +29,53 @@ class Timer {
         }
 };
 
-class BoardCell {
+class GridBlock {
     public:
         Color color;
         bool isEmpty;
 
-        BoardCell(Color _color, bool _isEmpty): color(_color), isEmpty(_isEmpty) {}
+        GridBlock(Color _color, bool _isEmpty): color(_color), isEmpty(_isEmpty) {}
+};
+
+class GameGrid {
+    private:
+        std::vector<std::vector<GridBlock>> grid;
+    
+    public:
+        GameGrid() {
+            for (int i = 0; i < GRID_WIDTH; i++) {
+                grid.push_back(std::vector<GridBlock>(GRID_HEIGHT, GridBlock(BLANK, true)));
+            }
+        }
+
+        bool isEmpty(Position p) {
+            return this->grid[p.x][p.y].isEmpty;
+        }
+
+        Color getColor(Position p) {
+            return this->grid[p.x][p.y].color;
+        }
+
+        bool checkCollision(Position p) {
+            if (!this->isEmpty(p)) {
+                return true;
+            }
+            if (p.x < 0 or p.x >= GRID_WIDTH) {
+                return true;
+            }
+            if (p.y >= GRID_HEIGHT) {
+                return true;
+            }
+            return false;
+        }
+
+        void setBlock(Position p, Color color) {
+            this->grid[p.x][p.y] = GridBlock(color, false);
+        }
+
+        void clearBlock(Position p) {
+            this->grid[p.x][p.y] = GridBlock(BLANK, true);
+        }
 };
 
 int main(void) { 
@@ -42,27 +83,26 @@ int main(void) {
     Timer timer;
     SetTargetFPS(60);
     Position pos(0, 0);
-
-    std::vector<std::vector<BoardCell>> grid;
-    for (int i = 0; i < GRID_WIDTH; i++) {
-        grid.push_back(std::vector<BoardCell>(GRID_HEIGHT, BoardCell(BLANK, true)));
-    }
+    GameGrid grid;
 
     while (!WindowShouldClose()) {
         if (timer.getElapsed() > 1) {
-            if (pos.y == 9) {
-                grid[pos.x][pos.y] = BoardCell(RED, false);
-                pos.y = 0;
-            }
-            else if (grid[pos.x][pos.y + 1].isEmpty) {
-                pos.y++;
-            }
-            else if (!grid[pos.x][pos.y].isEmpty) {
+            Position newPos(pos.x, pos.y + 1);
+
+            if (grid.checkCollision(pos)) {
                 // game over
+                continue;
             }
+            // active piece moving down would cause a position
+            // place it at its current position and start a new active piece
+            if (grid.checkCollision(newPos)) {
+                grid.setBlock(pos, RED);
+                pos = Position(0, 0);
+            }
+            // moving the active piece down does not cause a collision
+            // so set its current position to the new position
             else {
-                grid[pos.x][pos.y] = BoardCell(RED, false);
-                pos.y = 0;
+                pos = newPos;
             }
 
             timer.start();
@@ -75,9 +115,9 @@ int main(void) {
 
             for (int x = 0; x < GRID_WIDTH; x++) {
                 for (int y = 0; y < GRID_HEIGHT; y++) {
-                    BoardCell cell = grid[x][y];
-                    if (!cell.isEmpty) {
-                        DrawRectangle(25 * x, 25 * y, 25, 25, RED);
+                    Position p(x, y);
+                    if (!grid.isEmpty(p)) {
+                        DrawRectangle(25 * x, 25 * y, 25, 25, grid.getColor(p));
                         DrawRectangleLines(25 * x, 25 * y, 25, 25, BLACK);
                     }
                 }
